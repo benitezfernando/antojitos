d59b57d8e429fef089247632c45a5ffa59cc1c04
Fernando <fabenitez2@teco.com.ar>
2026-04-12 11:09:52 -0300

    feat: clean cache

--- src/components/ServiceWorkerRegistrar.tsx
+++ src/components/ServiceWorkerRegistrar.tsx
@@ -4,11 +4,20 @@ import { useEffect } from 'react';
 
 export default function ServiceWorkerRegistrar() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .catch((err) => console.error('[SW] Error al registrar:', err));
-    }
+    if (!('serviceWorker' in navigator)) return;
+
+    // Desregistrar todos los SW viejos y limpiar todos los caches
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((reg) => reg.unregister());
+    });
+    caches.keys().then((keys) => {
+      keys.forEach((key) => caches.delete(key));
+    });
+
+    // Registrar el SW nuevo
+    navigator.serviceWorker
+      .register('/sw.js')
+      .catch((err) => console.error('[SW] Error al registrar:', err));
   }, []);
 
   return null;
